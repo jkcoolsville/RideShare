69210fe585bbb574ccf795de5f80f30875afdfd2
jkcoolsville <jkcoolsville@tamu.edu>
2016-10-01 19:29:45 -0500

    Initial commit

---
+++ app/src/main/java/com/example/jacobjackson/helloworld/SecondActivity.java
@@ -0,0 +1,71 @@
+package com.example.jacobjackson.helloworld;
+
+import android.os.StrictMode;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+
+        StrictMode.setThreadPolicy(policy);
+
+        HttpClient httpclient = new DefaultHttpClient();
+        HttpGet httpget = new HttpGet("http://104.236.19.68/ride.cgi?data1=halp");
+        try {
+            HttpResponse response = httpclient.execute(httpget);
+            if (response != null) {
+                String line = "";
+                InputStream inputstream = response.getEntity().getContent();
+                line = convertStreamToString(inputstream);
+                Toast.makeText(this, line, Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Unable to complete your request", Toast.LENGTH_LONG).show();
+            }
+        } catch (ClientProtocolException e) {
+            Toast.makeText(this, "Caught ClientProtocolException", Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            Toast.makeText(this, "Caught IOException", Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            Toast.makeText(this, "Caught Exception" + e.toString(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private String convertStreamToString(InputStream is) {
+        String line = "";
+        StringBuilder total = new StringBuilder();
+        BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+        try {
+            while ((line = rd.readLine()) != null) {
+                total.append(line);
+            }
+        } catch (Exception e) {
+            Toast.makeText(this, "Stream Exception", Toast.LENGTH_SHORT).show();
+        }
+        return total.toString();
+    }
+
+}
